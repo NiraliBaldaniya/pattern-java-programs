06898e46d453c0ab5fa8aa18f5989153dba6f93e
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-06 17:07:26 +0530

    add diamonde star pattern

---
+++ diamond.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class diamond{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number :");
+        int n =sc.nextInt();
+
+        int  st=n/2+1;
+        int sp=1;
+
+        for(int i=1;i<=n;i++) 
+        {
+            for (int j = 1; j <= st; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= sp; j++)
+            {
+                System.out.print("*");
+            }
+
+            if(i<=n/2)
+            
+            {
+                sp+=2;
+                st--;
+            }
+
+            else
+            {
+                sp-=2;
+                st++;
+            }
+
+
+            System.out.println();
+        }
+    }
+
+}
